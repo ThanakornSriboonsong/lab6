7ab0241eabb18753511d6293ccc58f34a0a79b95
Pleum <plemplemplemplem5@gmail.com>
2024-11-29 16:38:20 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 int main(){
+    int a = 1 ;
+    int b = 0 ;
+    int c = 0 ;
+
+    while( a != 0 ){
+    
     cout << "Enter an integer: ";
+    cin >> a ;
+    if( a %2 == 0){
+        b++ ;
+    }else if(a%2 == 1) {
+        c++ ;
+    }
+    
+    
+    
+    }
     cout << "#Even numbers = ";
+    cout << b-1<< endl ;
     cout << "#Odd numbers = ";
+    cout << c ;
+   
     return 0;
 }
